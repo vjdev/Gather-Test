68768467d401b15474a2b1f9ea9e26211bc4bf05
vijeesh <vijeesh.k@care.com>
2021-04-02 17:16:06 +0530

    tableview setup; tableview methods added

--- Gather-Test/Views/GatheredListViewController.swift
+++ Gather-Test/Views/GatheredListViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 class GatheredListViewController: UIViewController {
 
     @IBOutlet private weak var gatheredListTableview: UITableView!
-    
+    private let cellIdentifier = "cell-identifier"
     override func viewDidLoad() {
         super.viewDidLoad()
             
     }
+    
+    private func setupTableview() {
+        registerTableView()
+        gatheredListTableview.delegate = self
+        gatheredListTableview.dataSource = self
+    }
+    
+    private func registerTableView() {
+        gatheredListTableview.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
 }
-
+// MARK: - Tableview
+extension GatheredListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        return cell!
+    }
+}
