c2aa21da9c9cfdbe34ef90b38f26705d4fb6109e
vijeesh <vijeesh.k@care.com>
2021-04-02 18:09:36 +0530

    tableview loaded with dummy data

--- Gather-Test/Views/GatheredListViewController.swift
+++ Gather-Test/Views/GatheredListViewController.swift
@@ -11,6 +11,7 @@ final class GatheredListViewController: UIViewController {
 
     @IBOutlet private weak var gatheredListTableview: UITableView!
     private let cellIdentifier = "cell-identifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,11 +31,12 @@ final class GatheredListViewController: UIViewController {
 // MARK: - Tableview
 extension GatheredListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        cell?.textLabel?.text = "\(indexPath.row)"
         return cell!
     }
 }
