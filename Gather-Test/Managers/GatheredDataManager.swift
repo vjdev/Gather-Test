3ae0945a40891402024901b4a834405d5ead7015
vijeesh <vijeesh.k@care.com>
2021-04-02 18:09:13 +0530

    manager class added

---
+++ Gather-Test/Managers/GatheredDataManager.swift
@@ -0,0 +1,18 @@
+//
+//  GatheredDataManager.swift
+//  Gather-Test
+//
+//  Created by vijeesh.k on 02/04/21.
+//
+
+import Foundation
+
+final class GatheredDataManager {
+    static let sharedInstance = GatheredDataManager()
+    
+    private init() {
+    }
+    
+    
+    
+}
