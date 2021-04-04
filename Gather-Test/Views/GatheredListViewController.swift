fd8c25c7031cd73f9a9aaeb4ea6ac74431cb38ca
vijeesh <vijeesh.k@care.com>
2021-04-04 15:06:10 +0530

    loading image names in tableview

--- Gather-Test/Views/GatheredListViewController.swift
+++ Gather-Test/Views/GatheredListViewController.swift
@@ -11,11 +11,16 @@ final class GatheredListViewController: UIViewController {
 
     @IBOutlet private weak var gatheredListTableview: UITableView!
     private let cellIdentifier = "cell-identifier"
-    
+    private var gatheredDataArray: [GatheredData]? {
+        didSet {
+            gatheredListTableview.reloadData()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupTableview()
+        gatheredDataArray = GatheredDataManager().getGatheredData()
     }
     
     private func setupTableview() {
@@ -28,15 +33,17 @@ final class GatheredListViewController: UIViewController {
         gatheredListTableview.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
     }
 }
+
 // MARK: - Tableview
 extension GatheredListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return gatheredDataArray?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
-        cell?.textLabel?.text = "\(indexPath.row)"
+        let gatheredData = gatheredDataArray?[indexPath.row]
+        cell?.textLabel?.text = gatheredData?.imageName
         return cell!
     }
 }
