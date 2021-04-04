2253228728e0154b43f5930330b13716099d0b47
vijeesh <vijeesh.k@care.com>
2021-04-04 15:05:54 +0530

    data manager class added

--- Gather-Test/Managers/GatheredDataManager.swift
+++ Gather-Test/Managers/GatheredDataManager.swift
@@ -8,11 +8,32 @@
 import Foundation
 
 final class GatheredDataManager {
-    static let sharedInstance = GatheredDataManager()
     
-    private init() {
+    private let totalFiles = 24
+    
+    func getGatheredData() -> [GatheredData]? {
+        var gatheredDataArray = [GatheredData]()
+        for fileIndex in 0..<totalFiles {
+            let fileName = getFileName(index: fileIndex)
+            let path = Bundle.main.path(forResource: fileName, ofType: "json")
+            let jsonData = try! String(contentsOfFile: path!).data(using: .utf8)!
+            if let itemDetails = try? JSONDecoder().decode([ItemDetails].self, from: jsonData) {
+                var gatheredData = GatheredData()
+                gatheredData.imageName = fileName
+                gatheredData.itemDetails = itemDetails
+                gatheredDataArray.append(gatheredData)
+            }
+        }
+        return gatheredDataArray
     }
     
-    
-    
+    func getFileName(index: Int) -> String {
+        if index < 10 {
+            return "img000\(index)"
+        }
+        return "img00\(index)"
+    }
 }
+
+
+
