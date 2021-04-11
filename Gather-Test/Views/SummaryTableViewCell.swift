cc982a95c7b93e9235d34a9fecf22edc628b27e8
vijeesh <vijeesh.k@care.com>
2021-04-11 14:46:04 +0530

    summary tableview implementation

---
+++ Gather-Test/Views/SummaryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SummaryTableViewCell.swift
+//  Gather-Test
+//
+//  Created by vijeesh.k on 10/04/21.
+//
+
+import UIKit
+
+class SummaryTableViewCell: UITableViewCell {
+    @IBOutlet private weak var codeLabel: UILabel!
+    @IBOutlet private weak var countLabel: UILabel!
+    
+    override func prepareForReuse() {
+        codeLabel.text = nil
+        countLabel.text = nil
+    }
+    
+    func configureWith(code: String, count: Int) {
+        codeLabel.text = code
+        countLabel.text = "\(count)"
+    }
+}
--- Gather-Test/Views/SummaryViewController.swift
+++ Gather-Test/Views/SummaryViewController.swift
@@ -7,8 +7,14 @@
 
 import UIKit
 
-class SummaryViewController: UIViewController {
+final class SummaryViewController: UIViewController {
 
+    @IBOutlet weak var summaryTableView: UITableView!
+    
+    let cellIdentifier = "SummaryTableViewCell"
+    var summary = Dictionary<String?, Int>()
+    var summaryItemCodes = [String?]()
+    
     static func viewController() -> SummaryViewController {
         let storyBoard  = UIStoryboard.init(name: "Main", bundle: Bundle.main)
         let viewController = storyBoard.instantiateViewController(identifier: "\(SummaryViewController.self)") as! SummaryViewController
@@ -17,5 +23,48 @@ class SummaryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupTableView()
+        registerTableView()
+        displaySummaryDetails()
+    }
+    
+    private func setupTableView() {
+        summaryTableView.dataSource = self
+        summaryTableView.delegate = self
+    }
+    
+    private func registerTableView() {
+        let nib = UINib(nibName: "\(SummaryTableViewCell.self)", bundle: nil)
+        summaryTableView.register(nib, forCellReuseIdentifier: cellIdentifier)
+    }
+    
+    private func displaySummaryDetails() {
+        guard let summaryArray = GatheredDataManager.sharedInstance.getGatheredItemSummary() else {
+            return
+        }
+        
+        for item in summaryArray {
+            summaryItemCodes.append(item.code)
+        }
+
+        let mappedItems = summaryItemCodes.map { ($0, 1) }
+        summary = Dictionary(mappedItems, uniquingKeysWith: +)
+        summaryTableView.reloadData()
     }
 }
+
+extension SummaryViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return summary.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! SummaryTableViewCell
+        if let code = summaryItemCodes[indexPath.row], let count = summary[code] {
+            cell.configureWith(code: code, count: count)
+        }
+        return cell
+    }
+}
+
