a9fb12723d51dbb15d0a519f8eab071944910883
vijeesh <vijeesh.k@care.com>
2021-04-12 19:47:11 +0530

    colors added to constant file

--- Gather-Test/Constants/Constants.swift
+++ Gather-Test/Constants/Constants.swift
@@ -5,8 +5,10 @@
 //  Created by vijeesh.k on 04/04/21.
 //
 
-import Foundation
+import UIKit
 
 struct Constants {
     static let placeholderImage = "placeholder-image"
+    static let barCodeRectangleColor: UIColor = .yellow
+    static let boxRectangleColor: UIColor = .magenta
 }
